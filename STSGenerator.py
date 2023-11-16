9caac08f89f77eb72d7c27b1d2ae89174a8592cb
Joshua Ganschow <ganschowjosh@gmail.com>
2023-11-16 14:03:54 -0600

    Generating keys, still slow though

--- STSGenerator.py
+++ STSGenerator.py
@@ -1,4 +1,8 @@
 import random
+import hashlib
+import json
+from itertools import permutations
+import time
 
 # Get the order of the desired system from user input
 v = int(input("Enter order of desired system:\t"))
@@ -156,6 +160,42 @@ def ConstructBlocks(v, Other):
                 B.append({x, y, z})
     return B
 
+def canonicalForm(S, T):
+    startTime = time.time()
+    # Generate all permutations of S
+    perms = permutations(S)
+    
+    # Initialize the canonical form to be the lexicographically first triple system
+    canonicalT = sorted(sorted(triple) for triple in T)
+    
+    for perm in perms:
+        # Create a mapping from the original points to the permuted points
+        alpha = {S[i]: perm[i] for i in range(len(S))}
+        
+        # Apply the permutation to T
+        tPerm = [{alpha[point] for point in triple} for triple in T]
+        tPerm = sorted(sorted(triple) for triple in tPerm)
+
+        
+        # Update the canonical form if necessary
+        if tPerm < canonicalT:
+            canonicalT = tPerm
+    
+    print(f"Time taken to find canonical form: {time.time() - startTime:.5f} seconds")
+
+    return canonicalT
+
+def generateKey(S, T):
+    # Generate the canonical form of the STS
+    canonicalT = canonicalForm(S, T)
+    
+    # Serialize the canonical form
+    serializedT = json.dumps(canonicalT).encode('utf-8')
+    
+    # Generate a SHA-256 hash of the serialized canonical form
+    hashObject = hashlib.sha256(serializedT)
+    return hashObject.hexdigest()
+
 # Function to implement Revised Stinson's Algorithm
 def RevisedStinsonsAlgorithm(v):
     global NumBlocks, Other
@@ -166,6 +206,12 @@ def RevisedStinsonsAlgorithm(v):
         RevisedSwitch()
     B = ConstructBlocks(v, Other)
     print(B)
+    S = set()
+    for triple in B:
+        S.update(triple)
+    S = list(S)
+    key = generateKey(S, B)
+    print(key)
     print(isSteinerTripleSystem([n+1 for n in range(v)], B))
 
 # Check if the order is valid for a Steiner triple system
